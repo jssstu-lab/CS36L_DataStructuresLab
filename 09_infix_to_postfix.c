3599d1f1b00962316db873bef3d8ca86f2949725
Alaap Surendran <alaapsurendran@gmail.com>
2020-12-01 11:05:34 +0530

    Program 9 added

---
+++ 09_infix_to_postfix.c
@@ -0,0 +1,120 @@
+#include <stdio.h>
+#include <string.h>
+
+#define MAX_STACKARR_SIZE 50
+#define MAX_STRING_SIZE 200
+
+typedef struct stack {
+    int topIdx;
+    char items[MAX_STACKARR_SIZE];
+} Stack;
+
+void push(Stack* s, char op) {
+    if (s->topIdx == MAX_STACKARR_SIZE -1) {
+        printf("Stack full. Stack overflow. \n");
+        return;
+    }
+    s->items[++s->topIdx] = op;
+}
+
+char pop(Stack* s) {
+    if (s->topIdx == -1) {
+        printf("Stack empty. Stack underflow. \n");
+        return -1;
+    }
+    return s->items[s->topIdx--];
+}
+
+char peek(Stack *s) {
+    if (s->topIdx == -1) {
+        printf("Stack empty. Stack underflow. \n");
+        return -1;
+    }
+    return s->items[s->topIdx];
+}
+
+int isEmpty(Stack *s) {
+    return s->topIdx == -1;
+}
+
+int initStack(Stack* s) {
+    s->topIdx = -1;
+}
+
+int isOperator(char op) {
+    return op == '+' || op == '-' || op == '*' || op == '/' || op == '^' || op == '$' || op == '('; 
+}
+
+int inputPrecedence(char op) {
+    switch(op) {
+        case '(':
+            return 7;
+        case '^':
+        case '$':
+            return 6;
+        case '*':
+        case '/':
+            return 3;
+        case '+':
+        case '-':
+            return 1;
+    }
+}
+
+int stackPrecedence(char op) {
+    switch(op) {
+        case '^':
+        case '$':
+            return 5;
+        case '*':
+        case '/':
+            return 4;
+        case '+':
+        case '-':
+            return 2;
+        case '(':
+            return 0;
+    }
+}
+
+void infixToPostfix(char infix[], char postfix[]) {
+    Stack operatorStack;
+    initStack(&operatorStack);
+
+    int infixIdx = 0, postfixIdx = 0;
+    while (infix[infixIdx] != '\0') {
+        char inputChar = infix[infixIdx];
+        if (isOperator(inputChar)) {
+            if (isEmpty(&operatorStack) || (inputPrecedence(inputChar) > stackPrecedence(peek(&operatorStack)))) {
+                push(&operatorStack, inputChar);
+            } else {
+                while (!isEmpty(&operatorStack) && (inputPrecedence(inputChar) < stackPrecedence(peek(&operatorStack))))
+                    postfix[postfixIdx++] = pop(&operatorStack);
+                push(&operatorStack, inputChar);
+            }
+        } else if (infix[infixIdx] == ')') {
+            while (peek(&operatorStack) != '(')
+                postfix[postfixIdx++] = pop(&operatorStack);
+            pop(&operatorStack); // discard (
+        } else {
+            postfix[postfixIdx++] = infix[infixIdx];
+        }
+        infixIdx++;
+    }
+    while (!isEmpty(&operatorStack))
+        postfix[postfixIdx++] = pop(&operatorStack);
+
+    postfix[postfixIdx] = '\0';
+}
+
+int main() {
+    while (1) {
+        char infix[MAX_STRING_SIZE], postfix[MAX_STRING_SIZE];
+        printf("Enter infix expression (or 0 to exit) : ");
+        scanf("%s", infix);
+        if (strcmp(infix, "0") == 0)
+            break;
+        infixToPostfix(infix, postfix);
+        printf("%s\n", postfix);
+    }
+}
---
+++ 09_infix_to_prefix.c
@@ -0,0 +1,150 @@
+#include <stdio.h>
+#include <string.h>
+
+#define MAX_STACKARR_SIZE 50
+#define MAX_STRING_SIZE 200
+
+typedef struct stack {
+    int topIdx;
+    char items[MAX_STACKARR_SIZE][MAX_STRING_SIZE];
+} Stack;
+
+void push(Stack* s, char op[]) {
+    if (s->topIdx == MAX_STACKARR_SIZE -1) {
+        printf("Stack full. Stack overflow. \n");
+        return;
+    }
+    ++s->topIdx;
+    strcpy(s->items[s->topIdx], op);
+}
+
+void pop(Stack* s, char out[]) {
+    if (s->topIdx == -1) {
+        printf("Stack empty. Stack underflow. \n");
+        return;
+    }
+    strcpy(out, s->items[s->topIdx--]);
+}
+
+char* peek(Stack *s) {
+    if (s->topIdx == -1) {
+        printf("Stack empty. Stack underflow. \n");
+        return;
+    }
+    return s->items[s->topIdx];
+}
+
+int isEmpty(Stack *s) {
+    return s->topIdx == -1;
+}
+
+int initStack(Stack* s) {
+    s->topIdx = -1;
+}
+
+int isOperator(char op) {
+    return op == '+' || op == '-' || op == '*' || op == '/' || op == '^' || op == '$' || op == '('; 
+}
+
+int inputPrecedence(char op) {
+    switch(op) {
+        case '(':
+            return 7;
+        case '^':
+        case '$':
+            return 6;
+        case '*':
+        case '/':
+            return 3;
+        case '+':
+        case '-':
+            return 1;
+    }
+}
+
+int stackPrecedence(char op) {
+    switch(op) {
+        case '^':
+        case '$':
+            return 5;
+        case '*':
+        case '/':
+            return 4;
+        case '+':
+        case '-':
+            return 2;
+        case '(':
+            return 0;
+    }
+}
+
+void infixToPrefix(char infix[], char prefix[]) {
+    Stack operatorStack, operandStack;
+    initStack(&operatorStack);
+    initStack(&operandStack);
+
+    int infixIdx = 0;
+    while (infix[infixIdx] != '\0') {
+        char inputChar = infix[infixIdx];
+        if (isOperator(inputChar)) {
+            if (isEmpty(&operatorStack) || (inputPrecedence(inputChar) > stackPrecedence(peek(&operatorStack)[0]))) {
+                char opStr[] = " ";
+                opStr[0] = inputChar;
+                push(&operatorStack, opStr);
+            } else {
+                while (!isEmpty(&operatorStack) && (inputPrecedence(inputChar) < stackPrecedence(peek(&operatorStack)[0]))) {
+                    char oprand1[MAX_STRING_SIZE], oprand2[MAX_STRING_SIZE], op[MAX_STRING_SIZE];
+                    pop(&operandStack, oprand2);
+                    pop(&operatorStack, op);
+                    pop(&operandStack, oprand1);
+                    strcat(oprand1, oprand2);
+                    strcat(op, oprand1);
+                    push(&operandStack, op);
+                }
+                char opStr[] = " ";
+                opStr[0] = inputChar;
+                push(&operatorStack, opStr);
+            }
+        } else if (infix[infixIdx] == ')') {
+            while (peek(&operatorStack)[0] != '(') {
+                char oprand1[MAX_STRING_SIZE], oprand2[MAX_STRING_SIZE], op[MAX_STRING_SIZE];
+                pop(&operandStack, oprand2);
+                pop(&operatorStack, op);
+                pop(&operandStack, oprand1);
+                strcat(oprand1, oprand2);
+                strcat(op, oprand1);
+                push(&operandStack, op);
+            }
+            char discard[2];
+            pop(&operatorStack, discard); // discard (
+        } else {
+            char oprndStr[] = " ";
+            oprndStr[0] = inputChar;
+            push(&operandStack, oprndStr);
+        }
+        infixIdx++;
+    }
+    while (!isEmpty(&operatorStack)) {
+        char oprand1[MAX_STRING_SIZE], oprand2[MAX_STRING_SIZE], op[MAX_STRING_SIZE];
+        pop(&operandStack, oprand2);
+        pop(&operatorStack, op);
+        pop(&operandStack, oprand1);
+        strcat(oprand1, oprand2);
+        strcat(op, oprand1);
+        push(&operandStack, op);
+    }
+
+    pop(&operandStack, prefix);
+}
+
+int main() {
+    while (1) {
+        char infix[MAX_STRING_SIZE], prefix[MAX_STRING_SIZE];
+        printf("Enter infix expression (or 0 to exit) : ");
+        scanf("%s", infix);
+        if (strcmp(infix, "0") == 0)
+            break;
+        infixToPrefix(infix, prefix);
+        printf("%s\n", prefix);
+    }
+}
